e0ec2e0684ae14e2f44f2a2b88d86c430ce8ee0d
Anton <salvatoredl@mail.ru>
2018-03-27 20:31:34 +0800

    Lesson2 JS

--- lesson1js/js/script.js
+++ lesson1js/js/script.js
@@ -11,60 +11,57 @@ mainList = {
 var budget = mainList.budget;
 var shopGoods = mainList.shopGoods;
 
-for (var i = 0; i < 3; i++) {
- shopGoods[shopGoods.length] = prompt('Какой тип товаров будем продавать?');
+// Циклы фторого урока
+
+// for (var i = 0; i < 3; i++) {
+//  var a = prompt ("Какой тип товаров будем продавать?");
+//  // mainList.shopGoods[i] = a;
+//  if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50 ) {
+//   console.log("All true");
+//   mainList.shopGoods[i] = a;
+//  } else {
+//   i--;
+//   console.log("Not true");
+//  }
+// }
+
+// var i = 0;
+// while (i < 3) {
+// var a = prompt("Какой тип товаров будем продавать?")
+
+// if  ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50 ) {
+//  mainList.shopGoods[i] = a;
+//  i++;
+//  console.log("All true");
+// } else {
+//  console.log("Not True");
+// }
+// }
+
+var i = 0;
+do {
+ var a = prompt("Какой тип товаров будем продавать?");
+if  ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50 ) {
+ mainList.shopGoods[i] = a;
+ i++;
+ console.log("All true");
+} else {
+ console.log("Not True");
 }
+} while( i < 3);
+
+
+// Цикл с первого урока
+
+// for (var i = 0; i < 3; i++) {
+//  shopGoods[shopGoods.length] = prompt('Какой тип товаров будем продавать?');
+// }
+
+// Цикл с первого урока закончится тут!
 
 var dailyBudget = budget/30;
 
 alert("Бюджет на один день равен: " + dailyBudget);
 
-console.log(shopGoods);
+console.log(mainList);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// //Создаём переменные.
-// var budget;
-// var shopName;
-
-// //Присваиваем этим переменным значения, введённые пользователем.
-// budget = prompt("Ваш бюджет?");
-// console.log(budget);
-// shopName = prompt("Название вашего магазина?");
-// console.log(shopName);
