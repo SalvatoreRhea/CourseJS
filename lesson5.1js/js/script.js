eba146ba7cdac92dbacc58ef91c5d22735c46542
Anton <salvatoredl@mail.ru>
2018-04-02 22:23:23 +0800

    Lesson5.1

---
+++ lesson5.1js/js/script.js
@@ -0,0 +1,26 @@
+let btn = document.getElementById('open-btn'),
+    name = document.getElementsByClassName('name-value'),
+    budget = document.getElementsByClassName('budget-value'),
+    goods = document.getElementsByClassName('goods-value'),
+    items = document.getElementsByClassName('items-value'),
+    employers = document.getElementsByClassName('employers-value'),
+    discount = document.getElementsByClassName('discount-value'),
+    isopen = document.getElementsByClassName('isopen-value'),
+    goodsItem = document.getElementsByClassName('goods-item'),
+    button = document.getElementsByTagName('button'),
+    chooseItem = document.querySelector('.choose-item'),
+    time = document.querySelector('.time-value'),
+    count = document.querySelector('.count-budget-value'),
+    hireEmployersItem = document.querySelectorAll('.hire-employers-item');
+
+
+
+
+console.log(goodsItem);    
+console.log(button);    
+console.log(count);    
+console.log(hireEmployersItem);    
+  
+
+
+
