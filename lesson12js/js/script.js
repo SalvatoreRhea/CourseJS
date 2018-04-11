e1b634ff9356e5d7cabbd356f2cae2dcde248538
Anton <salvatoredl@mail.ru>
2018-04-11 22:06:20 +0800

    Lesson12JS

--- lesson12js/js/script.js
+++ lesson12js/js/script.js
@@ -1,18 +1,23 @@
 $(document).ready(function() {
- $('.main_btna').on('click', function() {
+ $('.main_btna, .main_btn').on('click', function() {
   $('.overlay').fadeIn();
- });
-
- $('.main_btna').on('click', function() {
   $('.modal').slideDown();
  });
 
  $('.close').on('click', function() {
   $('.modal').slideUp();
- });
-
- $('.close').on('click', function() {
   $('.overlay').fadeOut();
  });
+
+
+$('a[href ^= "#sheldure"]').on('click', function() {
+ $('.overlay').fadeIn();
+  $('.modal').slideDown();
+});
+
+
+
+ 
+
 
 });
